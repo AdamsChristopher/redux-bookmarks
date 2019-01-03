5e23388a4a5bec0c892d0ac58abecc2478b1120d
Christopher ADAMS <CJ.ADAMS@outlook.com>
2019-01-03 11:48:08 +0100

    Ajout du dossier containers + fichier BookmarkForm.js

---
+++ src/containers/BookmarkForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BookmarkForm extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default BookmarkForm;
