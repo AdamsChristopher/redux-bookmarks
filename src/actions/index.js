4f9c7a8863a0cac7aaa739f515b726edc35d253c
AdamsChristopher <neaderthalnoise@gmail.com>
2019-01-06 19:03:50 +0100

    Implémentation de l'édition d'un bookmark

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 export const ADD_BOOKMARK = 'ADD_BOOKMARK';
 export const DELETE_BOOKMARK = 'DELETE_BOOKMARK';
+export const UPDATE_BOOKMARK = 'UPDATE_BOOKMARK';
 
 export const addBookmark = (url, label) => ({
   type: ADD_BOOKMARK, url, label
@@ -7,4 +8,8 @@ export const addBookmark = (url, label) => ({
 
 export const deleteBookmark = id => ({
   type: DELETE_BOOKMARK, id
+});
+
+export const updateBookmark = (id, url, label) => ({
+  type: UPDATE_BOOKMARK, id, url, label
 });
